93319f3aaa4516d64f23bae46d9f6b2a099ad511
Baris Can Akkocak <bariscanakkocak@gmail.com>
2022-05-31 09:49:47 +0300

    "last commit before merge"

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<HomePage />} />
+        <Route path="/favourites" element={<HomePage />} />
+        <Route path="/cart" element={<HomePage />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-import {
-  GET_FILTERED_FETCH,
-  GET_PRODUCTS_FETCH,
-} from "../redux/actions/productsActions";
-import { RootState } from "../types/RootState.types";
-
+import Products from "../components/Products";
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const products = useSelector((state: RootState) => state.products.products);
-  const filtered = useSelector((state: any) => state.products.filteredProducts);
-
-
-  const clickHandler = () => {
-    dispatch({
-      type: GET_FILTERED_FETCH,
-      payload: { category: "jewelery" },
-    });
-  };
-
-  console.log(filtered);
-
   return (
     <div>
-      {/* {products.length !== 0 ? products.map((e: any) => e.title) : "loading"} */}
-      <button onClick={clickHandler}>FETCH FILTERED</button>
-      {filtered.length !== 0 ? filtered.map((e: any) => e.title) : "loading"}
+      <Products />
     </div>
   );
 };
-
 export default HomePage;
