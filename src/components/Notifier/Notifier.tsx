269a32b6d90aa16dfc2c03e53a474ef8991cd18a
Ugur Yildirim <yildirimugurfend@gmail.com>
2022-06-13 16:17:55 +0300

    notifier style update

--- src/components/Notifier/Notifier.tsx
+++ src/components/Notifier/Notifier.tsx
@@ -21,7 +21,7 @@ export default function Notifier() {
 
 
   return (
-    <div className={`${isShown ? "fixed right-[5%]" : "fixed right-[-100%]"} z-40 top-[20%]  xs:w-60 xs:h-12 p-3 rounded bg-header-main-color text-main-color flex items-center justify-between overflow-hidden transition-all ease-in`}>
+    <div className={`${isShown ? "fixed right-[5%]" : "fixed right-[-100%]"} z-40 top-[20%]  xs:w-60 xs:h-12 p-3 rounded bg-gradient-to-b from-main-color to-header-secondary-color hover:from-side-color hover:to-header-secondary-color text-header-main-color flex items-center justify-between overflow-hidden transition-all ease-in shadow-lg`}>
         <div className="w-full flex justify-between items-center">
             <span onClick={onClickHandler} className="cursor-pointer hover:text-side-color">x</span>
             <span className="w-[80%] text-right">{message ? message : "empty"}</span>
